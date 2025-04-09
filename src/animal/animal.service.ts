b1bb9940898df2d21b7efe0a3a62ee729b47e2ef
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-04-09 10:58:06 -0300

    Update animal.service.ts

--- src/animal/animal.service.ts
+++ src/animal/animal.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAnimalDto } from './dto/create-animal.dto';
 import { UpdateAnimalDto } from './dto/update-animal.dto';
+import { Animal } from './entities/animal.entity';
+import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 
 @Injectable()
 export class AnimalService {
-  create(createAnimalDto: CreateAnimalDto) {
-    return 'This action adds a new animal';
-  }
 
+  private animais: Animal[] = [
+    {
+        id: 1,
+        nome: "Animal 1",
+        raca: "Vira lata",
+        local: "Em frente a praça x",
+        doente: false
+       
+    }
+    
+  ]
+  
   findAll() {
-    return `This action returns all animal`;
+    return this.animais
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} animal`;
+    const animal = this.animais.find(animal => animal.id === id)
+
+    if (animal) return animal
+    throw new HttpException("Esse animal não existe!", HttpStatus.NOT_FOUND)
   }
 
+  create(createAnimalDto: CreateAnimalDto) {
+    const newId = this.animais.length +1
+
+    const newAnimal = {
+      id: newId,
+      ...createAnimalDto
+    }
+  }
+
+  
   update(id: number, updateAnimalDto: UpdateAnimalDto) {
-    return `This action updates a #${id} animal`;
+    const animalIndex = this.animais.findIndex(animal => animal.id === id)
+
+    if(animalIndex < 0)
+      throw new HttpException("Esse animal não existe!", HttpStatus.NOT_FOUND)
+
+    const animalItem = this.animais[animalIndex]
+
+    this.animais[animalIndex] ={
+      ...animalItem,
+      ...updateAnimalDto
+    }
+    return "Animal atualizado!"
   } 
 
   remove(id: number) {
-    return `This action removes a #${id} animal`;
+    const animalIndex = this.animais.findIndex(animal => animal.id === id)
+
+    if(animalIndex < 0)
+        throw new HttpException("Esse animal não existe!", HttpStatus.NOT_FOUND)
+
+    this.animais.splice(animalIndex, 1)
+
+    return "Animal deletado!"
   }
 }
