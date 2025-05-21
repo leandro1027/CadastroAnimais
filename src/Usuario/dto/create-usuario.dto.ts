0e6b692411b1c0dbb65e94eef0fe1ad7b5707c55
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-05-21 12:07:29 -0300

    usuario service e create usuario dto

--- src/Usuario/dto/create-usuario.dto.ts
+++ src/Usuario/dto/create-usuario.dto.ts
@@ -1,23 +1,22 @@
-import { IsEmail, IsNegative, IsNotEmpty, IsString, MaxLength } from "class-validator"
+import { IsBoolean, IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
-export class CreateUsuarioDto{
-   
-    @IsString({message: "O nome precisa ser um texto!"})
-    @MaxLength(20, {message: "O nome precisa ter no máximo 20 caracteres!"})
-    @IsNotEmpty({message: "O nome não pode ser vazio!"})
-    readonly nome: string
+export class CreateUsuarioDto {
+  @IsString({ message: 'O nome precisa ser um texto!' })
+  @MaxLength(20, { message: 'O nome deve ter no máximo 20 caracteres!' })
+  @IsNotEmpty({ message: 'O nome é obrigatório!' })
+  nome: string;
 
-    @MaxLength(11, {message: "O cpf precisa ter no máximo 11 digitos!"})
-    @IsNotEmpty({message: "O cpf é obrigatório!"})
-    readonly cpf: string
+  @IsString({ message: 'O CPF precisa ser um texto!' })
+  @MaxLength(11, { message: 'O CPF deve ter no máximo 11 dígitos!' })
+  @IsNotEmpty({ message: 'O CPF é obrigatório!' })
+  cpf: string;
 
-    @IsNotEmpty({message: "O endereço não pode ser vazio!"})
-    @MaxLength(60, {message: "O endereço precisa ter no máximo 60 caracteres!"})
-    readonly endereco: string
+  @IsEmail({}, { message: 'O e-mail precisa ser válido!' })
+  @MaxLength(30, { message: 'O e-mail deve ter no máximo 30 caracteres!' })
+  @IsNotEmpty({ message: 'O e-mail é obrigatório!' })
+  email: string;
 
-    @IsEmail()
-    @MaxLength(30, {message: "O e-mail precisa ter no máximo 25 caracteres!"})
-    @IsNotEmpty({message: "O e-mail é obrigatório!"})
-    readonly email: string
+  @IsBoolean({ message: 'O campo "ja_adotou" precisa ser verdadeiro ou falso!' })
+  @IsNotEmpty({ message: 'O campo "ja_adotou" é obrigatório!' })
+  ja_adotou: boolean;
 }
-   
--- src/Usuario/usuario.service.ts
+++ src/Usuario/usuario.service.ts
@@ -1,105 +1,84 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { usuario } from "./entities/usuario.entity";
-import { CreateUsuarioDto } from "./dto/create-usuario.dto";
-import { UpdateUsuarioDto } from "./dto/update-usuario.dto";
-import { PrismaService } from "src/prisma/prisma.service";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUsuarioDto } from './dto/create-usuario.dto';
+import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 
 @Injectable()
 export class UsuarioService {
+  constructor(private readonly prismaService: PrismaService) {}
 
-    private usuarios: usuario[] = [
-        {
-            id: 1,
-            nome: "Nome 1",
-            cpf: "1344444444",
-            endereco: "Algum",
-            email: "testando@gmail.com",
-            ja_adotou: false
-            
-           
-        }
-      ]
-      
-      constructor(private readonly prismaService: PrismaService){}
-
-      async findAll(){
-          const allUsuarios = await this.prismaService.usuario.findMany()
-          return allUsuarios
-      }
-      
-      async findOne(id: number){
-        const usuario = await this.prismaService.usuario.findFirst({
-            where: {
-                id:id
-            }
-        })
-
-        if(usuario?.nome) return usuario
-        throw new HttpException("Esse Usuário não existe!", HttpStatus.NOT_FOUND)
+  async findAll() {
+    try {
+      return await this.prismaService.usuario.findMany({
+        include: {
+          animaisAdotados: true, 
+        },
+      });
+    } catch (error) {
+      throw new HttpException('Erro ao buscar usuários!', HttpStatus.BAD_REQUEST);
     }
-      
-        async create(createusuarioDto: CreateUsuarioDto) {
-          try{
-            const newUsuarios = this.prismaService.usuario.create({
-              data:{
-                  nome: createusuarioDto.nome,
-                  cpf: createusuarioDto.cpf,
-                  email: createusuarioDto.email,
-                  ja_adotou: false
-              }
-          })
-          return newUsuarios
+  }
 
-        }catch(e){
-            throw new HttpException("Não foi possivel cadastrar o usuário!", HttpStatus.BAD_REQUEST)
-          }
-        }
-      
-        
-        async update(id: number, UpdateUsuarioDto: UpdateUsuarioDto){
-          try{
-           const findUsuario = await this.prismaService.usuario.findFirst({
-               where: {
-                   id: id
-               }
-           })
-           
-           if(!findUsuario)
-               throw new HttpException("Esse usuário não existe!", HttpStatus.NOT_FOUND)
-   
-           const usuario = await  this.prismaService.usuario.update({
-               where: {
-                   id: findUsuario.id
-               },
-               data: UpdateUsuarioDto
-           })
-           return usuario
-          } catch(e){
-           throw new HttpException("Não foi possivel atualizar a tarefa!", HttpStatus.BAD_REQUEST)
-          }
-   
-       }
-      
-       async delete(id: number){
-        try{
-            const findUsuario = await this.prismaService.usuario.findFirst({
-                where: {
-                    id: id
-                }
-            })
+  async findOne(id: number) {
+    const usuario = await this.prismaService.usuario.findUnique({
+      where: { id },
+      include: { animaisAdotados: true },
+    });
 
-            if(!findUsuario)
-                throw new HttpException("Esse usuário não existe!", HttpStatus.NOT_FOUND)
+    if (!usuario) {
+      throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+    }
 
-                await this.prismaService.usuario.delete({
-                    where:{
-                        id: findUsuario.id
-                    }
-                })
+    return usuario;
+  }
 
-                return "Usuário excluido com sucesso!"
-            }catch(e){
-                throw new HttpException("Não possivel deletar ao usuário!", HttpStatus.BAD_REQUEST)
-            }
-        }
+  async create(createUsuarioDto: CreateUsuarioDto) {
+    try {
+      const usuario = await this.prismaService.usuario.create({
+        data: createUsuarioDto,
+      });
+
+      return usuario;
+    } catch (error) {
+      throw new HttpException('Erro ao criar o usuário!', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
+    const usuarioExistente = await this.prismaService.usuario.findUnique({
+      where: { id },
+    });
+
+    if (!usuarioExistente) {
+      throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      const usuarioAtualizado = await this.prismaService.usuario.update({
+        where: { id },
+        data: updateUsuarioDto,
+      });
+
+      return usuarioAtualizado;
+    } catch (error) {
+      throw new HttpException('Erro ao atualizar o usuário!', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async delete(id: number) {
+    const usuarioExistente = await this.prismaService.usuario.findUnique({
+      where: { id },
+    });
+
+    if (!usuarioExistente) {
+      throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      await this.prismaService.usuario.delete({ where: { id } });
+      return { message: 'Usuário removido com sucesso!' };
+    } catch (error) {
+      throw new HttpException('Erro ao deletar o usuário!', HttpStatus.BAD_REQUEST);
+    }
+  }
 }
