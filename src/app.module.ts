d943ffbca51a88d06de190eace4c372ad23040ce
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-05-27 20:00:49 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuarioModule } from './Usuario/usuario.module';
+import { AnimalModule } from './Animal/animal.module';
 
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, AnimalModule],
   controllers: [AppController],
   providers: [AppService],
 
